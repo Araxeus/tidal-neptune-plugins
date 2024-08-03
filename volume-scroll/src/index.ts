d2b74782324556bf41df7c4f7b8b4abd14da45d5
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-08-04 02:18:01 +0300

    cleanup

--- volume-scroll/src/index.ts
+++ volume-scroll/src/index.ts
@@ -1,7 +1,4 @@
-import {
-    //intercept,
-    actions,
-} from '@neptune';
+import { actions } from '@neptune';
 
 const audioElement = document.querySelector<HTMLElement>(
     '#footerPlayer>[class^="moreContainer"]>[class^="sliderContainer"]',
@@ -18,59 +15,6 @@ function onWheel(e: WheelEvent) {
 
 audioElement?.addEventListener('wheel', onWheel);
 
-// This is where you would typically put cleanup code.
 export function onUnload() {
     audioElement?.removeEventListener('wheel', onWheel);
 }
-
-// import { settings } from './settings';
-
-// let enterNormalFullscreen: true | undefined = undefined;
-// const unloadInterceptAllowed = intercept('view/FULLSCREEN_ALLOWED', () => {
-//     if (enterNormalFullscreen || settings.useTidalFullscreen) {
-//         enterNormalFullscreen = undefined;
-//         return true;
-//     }
-//     return true;
-// });
-// const unloadInterceptRequest = intercept('view/REQUEST_FULLSCREEN', () => {
-//     enterNormalFullscreen = true;
-// });
-// const onKeyDown = (event: KeyboardEvent) => {
-//     if (event.key === 'F11') {
-//         event.preventDefault();
-//         document.fullscreenElement
-//             ? document.exitFullscreen()
-//             : document.documentElement.requestFullscreen();
-
-//         if (!settings.useTidalFullscreen && !document.fullscreenElement) {
-//             const bar = document.querySelector<HTMLElement>(
-//                 "div[class^='bar--']",
-//             );
-//             const contentContainer = document.querySelector<HTMLElement>(
-//                 "div[class^='mainContainer--'] > div[class^='containerRow--']",
-//             );
-
-//             if (bar !== null && contentContainer !== null) {
-//                 if (document.fullscreenElement) {
-//                     // Exiting fullscreen
-//                     contentContainer.style.maxHeight = '';
-//                     bar.style.display = '';
-//                     document.body.removeAttribute('is-fullscreen');
-//                 } else {
-//                     // Entering fullscreen
-//                     contentContainer.style.maxHeight = '100%';
-//                     bar.style.display = 'none';
-//                     document.body.setAttribute('is-fullscreen', '');
-//                 }
-//             }
-//         }
-//     }
-// };
-// window.addEventListener('keydown', onKeyDown);
-// export const onUnload = () => {
-//     unloadInterceptAllowed();
-//     unloadInterceptRequest();
-//     window.removeEventListener('keydown', onKeyDown);
-// };
-// export { Settings } from './settings';
