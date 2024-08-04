975d356b5af6dc5cad269e1e6fe2a3bf99038070
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-08-04 03:45:37 +0300

    v0.2.1

--- volume-scroll/release/index.js
+++ volume-scroll/release/index.js
@@ -1,16 +1,16 @@
-import{actions as p,store as T}from"@neptune";import{html as f}from"@neptune/voby";import{storage as i}from"@plugin";var g=(t)=>{i.settings??={};for(let o of Object.keys(t))i.settings[o]??=t[o];return i.settings};import{html as $,$ as b}from"@neptune/voby";import{html as v}from"@neptune/voby";var m=({children:t,tooltip:o})=>v`
+import{actions as s,store as c}from"@neptune";import{html as T}from"@neptune/voby";import{storage as p}from"@plugin";var a=(t)=>{p.settings??={};for(let o of Object.keys(t))p.settings[o]??=t[o];return p.settings};import{html as h,$ as S}from"@neptune/voby";import{html as b}from"@neptune/voby";var x=({children:t,tooltip:o})=>b`
 	<div style="margin-bottom: 15px;display: flex;justify-content: space-between;align-items: center;" title="${o}">${t}</div>
-`;var l=({value:t,onValue:o,title:r,tooltip:a})=>{const n=b(t),s=(u)=>o?.(u.target.value);return $`
-		<${m} tooltip=${a}>
-			<label for="text-${r}" style="font-size: 1.2em;margin-right: 16px;">${r}</label>
-			<input type="number" min="1" max="20" id="text-${r}" value=${n} onChange=${s} style="flex-grow: 1;" />
-            <input type="range" min="1" max="20" id="range-${r}" value=${n} onChange=${s} style="flex-grow: 1;" />
+`;var $=({value:t,onValue:o,title:e,tooltip:y})=>{const i=S(t),g=(n)=>o?.(n.target.value),m=(n)=>{n.preventDefault();const f=n.deltaY<0?1:-1,l=Math.min(20,Math.max(1,i()+f));i(l),o?.(l.toString())};return h`
+		<${x} tooltip=${y}>
+			<label for="text-${e}" style="font-size: 1.2em;margin-right: 16px;">${e}</label>
+			<input type="number" min="1" max="20" id="text-${e}" value=${i} onChange=${g} onwheel=${m} style="flex-grow: 1; max-width: 35px; margin-right: 8px;" />
+            <input type="range" min="1" max="20" id="range-${e}" value=${i} onChange=${g} onwheel=${m} style="flex-grow: 1;" />
 		<//>
-	`};var e=g({steps:10}),S=()=>f`<div>
-    	<${l}
-		value=${e.steps}
-		onValue=${(t)=>{e.steps=parseInt(t)}}
+	`};var r=a({steps:10}),d=()=>T`<div>
+    	<${$}
+		value=${r.steps}
+		onValue=${(t)=>{r.steps=parseInt(t)}}
 		title="Volume Steps"
 		tooltip="The volume percentage to adjust by"
 	/>
-</div>`;function y(t){if(!t.deltaY)return;t.preventDefault();const o=t.deltaY<0;if(e.steps===10)h(o);else d(o)}function h(t){t?p.playbackControls.increaseVolume():p.playbackControls.decreaseVolume()}function d(t){const o=T.getState().playbackControls.volume,r=Math.round(t?Math.min(o+e.steps,100):Math.max(o-e.steps,0));p.playbackControls.setVolume({volume:r})}function J(){x?.removeEventListener("wheel",y)}var x=document.querySelector('#footerPlayer>[class^="moreContainer"]>[class^="sliderContainer"]');x?.addEventListener("wheel",y);export{J as onUnload,S as Settings};
+</div>`;function v(t){if(!t.deltaY)return;t.preventDefault();const o=t.deltaY<0;if(r.steps===10)k(o);else w(o)}function k(t){t?s.playbackControls.increaseVolume():s.playbackControls.decreaseVolume()}function w(t){const o=c.getState().playbackControls.volume,e=Math.round(t?Math.min(o+r.steps,100):Math.max(o-r.steps,0));s.playbackControls.setVolume({volume:e})}function q(){u?.removeEventListener("wheel",v)}var u=document.querySelector('#footerPlayer>[class^="moreContainer"]>[class^="sliderContainer"]');u?.addEventListener("wheel",v);export{q as onUnload,d as Settings};
