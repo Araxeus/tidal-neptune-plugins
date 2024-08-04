784481585137b129bee7bba3a1d63e2ce44dbea1
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-08-04 04:30:57 +0300

    v0.2.6

--- volume-scroll/lib/components/number-input.ts
+++ volume-scroll/lib/components/number-input.ts
@@ -15,10 +15,21 @@ export const NumberRangeInput = ({
 }: NumberInputProps) => {
     const reactiveValue = $(value);
     const onChange = (event: Event) => {
-        if (parseInt((event.target as HTMLInputElement).value) < 1) {
+        const newValue = (event.target as HTMLInputElement).value;
+        // VALIDATOR
+        if (
+            Number.isNaN(newValue) ||
+            parseInt((event.target as HTMLInputElement).value) < 1
+        ) {
             // @ts-expect-error
             event.target.value = '1';
             reactiveValue(1);
+            return;
+        }
+        if (parseInt((event.target as HTMLInputElement).value) > 20) {
+            // @ts-expect-error
+            event.target.value = '20';
+            reactiveValue(20);
             return;
         }
         onValue?.((event.target as HTMLInputElement).value);
@@ -35,7 +46,7 @@ export const NumberRangeInput = ({
     return html`
 		<${DivWithTooltip} tooltip=${tooltip}>
 			<label for="text-${title}" style="font-size: 1.2em;margin-right: 16px;">${title}</label>
-			<input type="number" class="neptune-text-input" min="1" max="20" id="text-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; max-width: 25px; margin-right: 8px; margin-top: 6px;" />
+			<input type="text" class="neptune-text-input" min="1" max="20" id="text-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; max-width: 25px; margin-right: 8px; margin-top: 6px;" />
             <input type="range" min="0" max="20" id="range-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; margin-top: 4px; accent-color: var(--wave-color-solid-accent-fill);" />
 		<//>
 	`;
--- volume-scroll/release/index.js
+++ volume-scroll/release/index.js
@@ -1,16 +1,16 @@
-import{actions as s,store as d}from"@neptune";import{html as T}from"@neptune/voby";import{storage as p}from"@plugin";var m=(t)=>{p.settings??={};for(let e of Object.keys(t))p.settings[e]??=t[e];return p.settings};import{html as h,$ as S}from"@neptune/voby";import{html as b}from"@neptune/voby";var x=({children:t,tooltip:e})=>b`
+import{actions as a,store as d}from"@neptune";import{html as S}from"@neptune/voby";import{storage as s}from"@plugin";var u=(t)=>{s.settings??={};for(let e of Object.keys(t))s.settings[e]??=t[e];return s.settings};import{html as b,$ as h}from"@neptune/voby";import{html as T}from"@neptune/voby";var x=({children:t,tooltip:e})=>T`
 	<div style="margin-bottom: 15px;display: flex;justify-content: space-between;align-items: center;" title="${e}">${t}</div>
-`;var u=({value:t,onValue:e,title:o,tooltip:$})=>{const i=S(t),g=(r)=>{if(parseInt(r.target.value)<1){r.target.value="1",i(1);return}e?.(r.target.value)},l=(r)=>{r.preventDefault();const y=r.deltaY<0?1:-1,a=Math.min(20,Math.max(1,i()+y));i(a),e?.(a.toString())};return h`
-		<${x} tooltip=${$}>
-			<label for="text-${o}" style="font-size: 1.2em;margin-right: 16px;">${o}</label>
-			<input type="number" class="neptune-text-input" min="1" max="20" id="text-${o}" value=${i} onChange=${g} onwheel=${l} style="flex-grow: 1; max-width: 25px; margin-right: 8px; margin-top: 6px;" />
-            <input type="range" min="0" max="20" id="range-${o}" value=${i} onChange=${g} onwheel=${l} style="flex-grow: 1; margin-top: 4px; accent-color: var(--wave-color-solid-accent-fill);" />
+`;var f=({value:t,onValue:e,title:r,tooltip:y})=>{const n=h(t),g=(o)=>{const p=o.target.value;if(Number.isNaN(p)||parseInt(o.target.value)<1){o.target.value="1",n(1);return}if(parseInt(o.target.value)>20){o.target.value="20",n(20);return}e?.(o.target.value)},l=(o)=>{o.preventDefault();const p=o.deltaY<0?1:-1,m=Math.min(20,Math.max(1,n()+p));n(m),e?.(m.toString())};return b`
+		<${x} tooltip=${y}>
+			<label for="text-${r}" style="font-size: 1.2em;margin-right: 16px;">${r}</label>
+			<input type="text" class="neptune-text-input" min="1" max="20" id="text-${r}" value=${n} onChange=${g} onwheel=${l} style="flex-grow: 1; max-width: 25px; margin-right: 8px; margin-top: 6px;" />
+            <input type="range" min="0" max="20" id="range-${r}" value=${n} onChange=${g} onwheel=${l} style="flex-grow: 1; margin-top: 4px; accent-color: var(--wave-color-solid-accent-fill);" />
 		<//>
-	`};var n=m({steps:10}),c=()=>T`<div>
-    	<${u}
-		value=${n.steps}
-		onValue=${(t)=>{n.steps=parseInt(t)}}
+	`};var i=u({steps:10}),c=()=>S`<div>
+    	<${f}
+		value=${i.steps}
+		onValue=${(t)=>{i.steps=parseInt(t)}}
 		title="Volume Steps"
 		tooltip="The volume percentage to adjust by"
 	/>
-</div>`;function f(t){if(!t.deltaY)return;t.preventDefault();const e=t.deltaY<0;if(n.steps===10)w(e);else k(e)}function w(t){t?s.playbackControls.increaseVolume():s.playbackControls.decreaseVolume()}function k(t){const e=d.getState().playbackControls.volume,o=Math.round(t?Math.min(e+n.steps,100):Math.max(e-n.steps,0));s.playbackControls.setVolume({volume:o})}function E(){v?.removeEventListener("wheel",f)}var v=document.querySelector('#footerPlayer>[class^="moreContainer"]>[class^="sliderContainer"]');v?.addEventListener("wheel",f);export{E as onUnload,c as Settings};
+</div>`;function $(t){if(!t.deltaY)return;t.preventDefault();const e=t.deltaY<0;if(i.steps===10)N(e);else w(e)}function N(t){t?a.playbackControls.increaseVolume():a.playbackControls.decreaseVolume()}function w(t){const e=d.getState().playbackControls.volume,r=Math.round(t?Math.min(e+i.steps,100):Math.max(e-i.steps,0));a.playbackControls.setVolume({volume:r})}function z(){v?.removeEventListener("wheel",$)}var v=document.querySelector('#footerPlayer>[class^="moreContainer"]>[class^="sliderContainer"]');v?.addEventListener("wheel",$);export{z as onUnload,c as Settings};
