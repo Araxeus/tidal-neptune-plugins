916bf93eb40e3168510f936d346eafd8ae012500
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-08-04 04:23:44 +0300

    update settings style

--- volume-scroll/lib/components/number-input.ts
+++ volume-scroll/lib/components/number-input.ts
@@ -35,8 +35,8 @@ export const NumberRangeInput = ({
     return html`
 		<${DivWithTooltip} tooltip=${tooltip}>
 			<label for="text-${title}" style="font-size: 1.2em;margin-right: 16px;">${title}</label>
-			<input type="number" min="1" max="20" id="text-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; max-width: 35px; margin-right: 8px;" />
-            <input type="range" min="0" max="20" id="range-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1;" />
+			<input type="number" class="neptune-text-input" min="1" max="20" id="text-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; max-width: 25px; margin-right: 8px; margin-top: 6px;" />
+            <input type="range" min="0" max="20" id="range-${title}" value=${reactiveValue} onChange=${onChange} onwheel=${onWheel} style="flex-grow: 1; margin-top: 4px; accent-color: var(--wave-color-solid-accent-fill);" />
 		<//>
 	`;
 };
